d574c0c7a9b821959ec11fb271ae167228e27e61
HANYONG LEE <glhy0718@gmail.com>
2020-02-11 16:31:50 -0800

    detection data shown by gauge add email settings

--- octoprint_badprint/__init__.py
+++ octoprint_badprint/__init__.py
@@ -4,23 +4,34 @@ from __future__ import absolute_import
 import octoprint.plugin
 import octoprint.settings
 
+
 class BadprintPlugin(octoprint.plugin.TemplatePlugin,
-					   octoprint.plugin.SettingsPlugin,
-					   octoprint.plugin.AssetPlugin):
-	def get_settings_defaults(self):
-		return dict(
-			stream = octoprint.settings.Settings().get(["webcam", "stream"])
-		)
+                     octoprint.plugin.SettingsPlugin,
+                     octoprint.plugin.AssetPlugin):
+    def get_settings_defaults(self):
+        return dict(
+            detect_server_url="http://localhost:3333",
 
-	def get_template_configs(self):
-		return [
-			dict(type="settings", custom_bindings=False)
-		]
+            sender_mail_server='smtp.gmail.com',
+            sender_mail_port=465,
+            sender_mail_username='',
+            sender_mail_password='',
+            sender_use_tls=False,
+            sender_use_ssl=True,
 
-	def get_assets(self):
-		return dict(
-			js=["js/badprint.js"]
-		)
+			recipicent_mail_address=''
+        )
+
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=False)
+        ]
+
+    def get_assets(self):
+        return dict(
+            js=["js/badprint.js"]
+        )
+
 
 __plugin_name__ = "badprint"
 __plugin_implementation__ = BadprintPlugin()
--- octoprint_badprint/static/js/badprint.js
+++ octoprint_badprint/static/js/badprint.js
@@ -1,24 +1,61 @@
-$(function() {
+$(function () {
 	function BadprintViewModel(parameters) {
-		var self = this;
-		self.settings = parameters[0];
-		
-		self.cancel = function(){
+		var self = this
+		self.settings = parameters[0]
+
+		self.cancel = function () {
 			showConfirmationDialog({
 				message: gettext("This will cancel your print."),
 				cancel: gettext("No"),
 				proceed: gettext("Yes"),
-				onproceed: function() {
-					OctoPrint.job.cancel();
+				onproceed: function () {
+					OctoPrint.job.cancel()
 				},
 				nofade: true
-			});
+			})
 		}
+
+		self.ajaxtest = function () {
+			var snapshot_url = self.settings.webcam_snapshotUrl() + '?' + Date.now()
+			var detect_server_url = self.settings.settings.plugins.badprint.detect_server_url()
+			var destination = detect_server_url + "/p/?img=" + snapshot_url
+			console.log(destination)
+			OctoPrint.ajax("GET", destination,
+				{
+					jsonpCallback: "callback",
+					dataType: 'jsonp',
+					crossDomain: true
+				}).done(function (data) {
+					console.log(data)
+					var detection_max = data.detections[0]
+					if (detection_max==undefined)
+						$('#badprint_gauge').attr('data-value', 0)
+					else{
+						console.log(detection_max)
+						$('#badprint_gauge').attr('data-value', detection_max[1])
+					}
+
+					self.ajaxtest()
+				}).fail(function (err) {
+					console.log("fail")
+					console.log(err)
+				})
+		}
+
+		self.onStartupComplete = function () {
+			console.log(self.settings.settings.plugins.badprint)
+
+			var stream_url = self.settings.webcam_streamUrl() + '?' + Date.now()
+			$("#badprint_webcam").append('<img src=\'' + stream_url + '\'>')
+
+			self.ajaxtest()
+		}
+
 	}
 
 	OCTOPRINT_VIEWMODELS.push([
 		BadprintViewModel,
 		["settingsViewModel"],
 		["#tab_plugin_badprint"]
-	]);
-});
+	])
+})
